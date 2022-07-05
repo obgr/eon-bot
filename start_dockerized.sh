f2b000cc8573664628407823a4b96cd4ee002613
Olle <hugo.berggren@gmail.com>
2022-07-06 01:18:55 +0200

    Update start_dockerized.sh

--- start_dockerized.sh
+++ start_dockerized.sh
@@ -4,7 +4,7 @@ CONTAINERNAME="eon-bot"
 IMAGEVER="1.0"
 
 # Build Image
-docker build -t ${CONTAINERNAME}:${IMAGEVER}
+docker build . -t ${CONTAINERNAME}:${IMAGEVER}
 # Run Image
 docker run -d \
  --mount type=bind,source=$PWD/src/.env,target=/app/src/.env,readonly \
