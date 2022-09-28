0b157b8d44520a319bb00bc67e4ac257f47eb8b0
Olle <olle@Jarred.localdomain>
2022-09-29 00:55:39 +0200

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,14 +4,17 @@
 source_venv() {
    . /eon-bot/venv/bin/activate
 }
+
 #get_latest_release() {
 #   curl --silent "https://api.github.com/repos/$1/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' # Consult github api and grep/sed out result
 #}
+
 #download_latest_release() {
 #   # Download release
 #   echo "Downloading $FILE from github."
 #   wget -q https://github.com/${REPO}/releases/download/${TAG}/${FILE}
 #}
+
 start_bot() {
    # Start Bot
    echo "Starting $FILE."
